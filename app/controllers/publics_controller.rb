63e27d4e248dd8598647df3c6c35b182d9df01ae
Hare Ram Rai <guru.java.ram@gmail.com>
2013-03-08 18:58:44 +0530

    using command line to get the download links

--- app/controllers/publics_controller.rb
+++ app/controllers/publics_controller.rb
@@ -1,14 +1,25 @@
 class PublicsController < ApplicationController
 
   def index
+
   end
 
   def create
 
-   url = params[:url]
-   require 'viddl-rb'
-   @download_urls = ViddlRb.get_urls(url)
+   begin
 
+     url = params[:url]
+     output =  `viddl-rb #{url} -u`
+     z = output.partition("\nhttp://")
+     @download_urls = z[2].split("\n")
+   rescue ViddlRb::DownloadError => e
+     @error = "Could not get download url: #{e.message}"
+   rescue ViddlRb::PluginError => e
+     @error = "Plugin blew up! #{e.message}\n" + "Backtrace:\n#{e.backtrace.join("\n")}"
+   rescue => e
+     @error = e.messag
+   end
+   
   end
 
 end
